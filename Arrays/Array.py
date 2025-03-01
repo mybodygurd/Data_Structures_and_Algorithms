e2bdba6121b501c1595f37ae06d575ba7a4bdae5
Mahdi <mybodygurd@gmail.com>
2025-03-01 19:03:04 +0330

    Initial commit of DSA solutions

---
+++ Arrays/Array.py
@@ -0,0 +1,56 @@
+class Array:
+    def __init__(self, initialSize):
+        self.__a = [None] * initialSize
+        self.__nItems = 0
+        
+    def __len__(self):
+        return self.__nItems
+    
+    def get(self, n):
+        if n >= 0 and n <= self.__nItems:
+            return self.__a[n]
+        
+    def set(self, n, value):
+        if n >= 0 and n <= self.__nItems:
+            self.__a[n] = value
+            
+    def insert(self, item):
+        self.__a[self.__nItems] = item
+        self.__nItems += 1
+        
+    def find(self, item):
+        for j in range(self.__nItems):
+            if self.__a[j] == item:
+                return j
+            
+        return -1
+    
+    def search(self, item):
+        return self.get(self.find(item))
+    
+    def delete(self, item):
+        for j in range(self.__nItems):
+            if self.__a[j] == item:
+                self.__nItems -= 1
+                for i in range(j, self.__nItems):
+                    self.__a[j] = self.__a[i + 1]
+                return True
+        return False
+    
+    def traverse(self, function=print):
+        for i in range(self.__nItems):
+            function(self.__a[i])
+            
+    def getMaxNum(self):
+        max_num = 0
+        contain_num = False
+        
+        for i in range(self.__nItems):    
+            if isinstance(self.__a[i], (int, float)):
+                contain_num = True
+                if self.__a[i] > max_num:
+                    max_num = self.__a[i]
+        if contain_num:
+            return max_num
+        else:
+            return None
---
+++ Arrays/ArrayClient.py
@@ -0,0 +1,31 @@
+from Array import Array
+
+MaxSize = 30
+arr = Array(MaxSize)
+str_arr = Array(MaxSize)
+items = [2, 98, 3, 7, 10, 88, 33, 12, 36, 68, 'string', 2.4, 0.01, 'Mahdi']
+
+for item in items:
+    arr.insert(item)
+
+print("Array containing", len(arr), "items")
+arr.traverse()
+
+print("search for 6 returns", arr.search(36))
+
+print("Deleting 88 returns", arr.delete(88))
+
+print("Setting item at index 3 to 33")
+arr.set(3, 33)
+
+print("Array after deletions has", len(arr), "items")
+arr.traverse()
+
+print("The highest number of array:", arr.getMaxNum())
+
+str_items = ['Mahdi', 'string', 'Python', 'data structure']
+
+for item in str_items:
+    str_arr.insert(item)
+    
+print('the highest number in string array returns', str_arr.getMaxNum())
